bc5b8b5a47e26fc907a71bee290082e0ca176542
unknown <hannanmalik1999@gmail.com>
2022-12-11 00:59:56 +0100

    Pic Cart Page Completed

--- src/Context.js
+++ src/Context.js
@@ -4,13 +4,14 @@ const Context = React.createContext()
 
 function ContextProvider({children}){
     const [allPhotos, setAllPhotos] = useState([])
+    const [cartItems, setCartItems] = useState([])
     const url = "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json"
     useEffect(() => {
         fetch(url)
         .then(res => res.json())
         .then(data => setAllPhotos(data))
     }, [])
-    
+
     function toggleFavorite(id) {
         const updatedArr = allPhotos.map(photo => {
             if(photo.id === id) {
@@ -23,9 +24,17 @@ function ContextProvider({children}){
         
         setAllPhotos(updatedArr)
     }
+
+    function addToCart(newItem) {
+        setCartItems(prevItems => [...prevItems, newItem])
+    }
+
+    function removeFromCart(id) {
+        setCartItems(prevItems => prevItems.filter(item => item.id !== id))
+    }
     
     return (
-        <Context.Provider value={{allPhotos, toggleFavorite}}>
+        <Context.Provider value={{allPhotos, toggleFavorite, cartItems, addToCart, removeFromCart}}>
             {children}
         </Context.Provider>
     )
