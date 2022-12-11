78a61e368d072490be108d6a616a145cc582b8b1
unknown <hannanmalik1999@gmail.com>
2022-12-11 01:14:45 +0100

    Cart Page Done

--- src/Context.js
+++ src/Context.js
@@ -1,22 +1,20 @@
 import React, {useState, useEffect} from "react"
 
 const Context = React.createContext()
-
 function ContextProvider({children}){
     const [allPhotos, setAllPhotos] = useState([])
     const [cartItems, setCartItems] = useState([])
+    
     const url = "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json"
     useEffect(() => {
         fetch(url)
-        .then(res => res.json())
-        .then(data => setAllPhotos(data))
+            .then(res => res.json())
+            .then(data => setAllPhotos(data))
     }, [])
-
+    
     function toggleFavorite(id) {
         const updatedArr = allPhotos.map(photo => {
             if(photo.id === id) {
-                console.log(id)
-                console.log(!photo.isFavorite)
                 return {...photo, isFavorite: !photo.isFavorite}
             }
             return photo
@@ -24,17 +22,28 @@ function ContextProvider({children}){
         
         setAllPhotos(updatedArr)
     }
-
+    
     function addToCart(newItem) {
         setCartItems(prevItems => [...prevItems, newItem])
     }
-
+    
     function removeFromCart(id) {
         setCartItems(prevItems => prevItems.filter(item => item.id !== id))
     }
     
+    function emptyCart() {
+        setCartItems([])
+    }
+    
     return (
-        <Context.Provider value={{allPhotos, toggleFavorite, cartItems, addToCart, removeFromCart}}>
+        <Context.Provider value={{
+            allPhotos, 
+            toggleFavorite, 
+            cartItems, 
+            addToCart, 
+            removeFromCart, 
+            emptyCart
+        }}>
             {children}
         </Context.Provider>
     )
